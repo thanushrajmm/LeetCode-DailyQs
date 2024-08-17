0a7b44599f67a6150e2ec84fb51c815a00a19b3e
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-17 20:32:47 +0530

    Create Q1. Find the Power of K-Size Subarrays I.java

---
+++ Biweekly Content 137/Q1. Find the Power of K-Size Subarrays I.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+      int n = nums.length;
+        int[] results = new int[n - k + 1];
+        
+        for (int i = 0; i <= n - k; i++) {
+            int max = nums[i];
+            boolean isSorted = true;
+            boolean areConsecutive = true;
+            
+            for (int j = i; j < i + k; j++) {
+                if (nums[j] > max) {
+                    max = nums[j];
+                }
+                if (j > i) {
+                    if (nums[j] < nums[j - 1]) {
+                        isSorted = false;
+                    }
+                    if (nums[j] != nums[j - 1] + 1) {
+                        areConsecutive = false;
+                    }
+                }
+            }
+            
+            if (isSorted && areConsecutive) {
+                results[i] = max;
+            } else {
+                results[i] = -1;
+            }
+        }
+        
+        return results;
+
+    }
+}
