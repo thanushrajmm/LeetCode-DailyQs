cbf87cfc67779112539998f0e245283075109ee9
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-19 14:22:47 +0530

    Create Aug19 - 2 Keys Keyboard.java

---
+++ August 2024/Aug19 - 2 Keys Keyboard.java
@@ -0,0 +1,19 @@
+problem link -> [ https://leetcode.com/problems/2-keys-keyboard ]
+class Solution {
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        
+        return steps;
+    }
+}
