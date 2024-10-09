67b8e7990b651e9ccb17f0e289e4862909873a6f
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 12:12:35 +0530

    Create Minimum Add to Make Parentheses Valid.java

---
+++ October 2024/Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,18 @@
+problem link -> [ https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/ ]
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open_c = 0;
+        int close_c = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open_c++;
+            } else if (c == ')' && open_c > 0) {
+                open_c--;
+            } else {
+                close_c++;
+            }
+        }
+        return open_c + close_c;
+    }
+}
